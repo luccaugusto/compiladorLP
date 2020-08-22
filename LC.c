c73385252fdf536aeaee1802ab2f5735372809e5
richvrm <richarddviniciuss99@gmail.com>
2020-08-22 11:06:52 -0300

    gitignore e funcoes vazias

--- LC.c
+++ LC.c
@@ -2,8 +2,33 @@
 #include <stdio.h>
 
 
+typedef struct{
+    int token;
+    int lexema;
+}Simbolos;
+
+//Simbolos simb[254];
+//char memoria[254][255];
+//int indice = 0;
+
+void adicionarRegistro(char *novoRegistro[]){
+    //memoria[indice] = novoRegistro[];
+   
+
+}
+
+void pesquisarRegistro(char p[255]){
+    //if(strcmp ("", p) == 0)
+
+}
+
+void adicionarReservados(){
+
+    
+}
+
 int main()
 {
-
+    adicionarReservados();
 	return 0;
 }
