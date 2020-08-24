e6b801da7a29954d1301bff8f0d88772f08336cf
lrr68 <luccaaugusto@protonmail.ch>
2020-08-24 12:07:34 -0300

    funcao de pesquisa

--- LC.c
+++ LC.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 #define TAM_TBL 254
+
+ /*parametros da linha de comando*/
+char *progFonte;
+char *progAsm;
 
 enum Tokens
 {
 	Identificador,
 };
-
- /*parametros da linha de comando*/
-char *progFonte;
-char *progAsm;
 
 /*registro do token na tabela*/
 struct Simbolo
@@ -30,10 +31,7 @@ struct Celula
 
 struct Celula *tabelaSimbolos[TAM_TBL];
 
-enum Tokens selecionarToken(char *str)
-{
-	return Identificador;
-}
+struct Celula *proc;
 
 unsigned int hash(unsigned char *str)
 {
@@ -46,82 +44,103 @@ unsigned int hash(unsigned char *str)
     return (hash % TAM_TBL);
 }
 
-void adicionarRegistro(char *novoRegistro)
+struct Celula *adicionarRegistro(char *lexema, int token)
 {
-
-	int token = selecionarToken(novoRegistro);
-	
-	unsigned int pos = hash(novoRegistro);
+	unsigned int pos = hash(lexema);
 	struct Celula *cel = (struct Celula *) malloc(sizeof(struct Celula));
 	struct Simbolo *simb = (struct Simbolo *) malloc(sizeof(struct Simbolo));
 	simb->token = token;
-	simb->lexema = novoRegistro;
+	simb->lexema = lexema;
 	simb->tipo = "int";
 	simb->mem = 0;
 	cel->prox = NULL;
 	cel->simbolo = *simb;
 	
-	if (tabelaSimbolos[pos]->simbolo.lexema == NULL) {
+	if (tabelaSimbolos[pos] == NULL) {
 		tabelaSimbolos[pos] = cel;
 	}else{
 		tabelaSimbolos[pos]->prox = cel;
 	}
-
+	return cel;
 }
 
-void pesquisarRegistro(char *p)
+struct Celula *pesquisarRegistro(char *lexema)
 {
+	int encontrado = 0;
+	int i = 0;
+	char *lexAtual;
+	struct Celula *retorno = NULL;
+	while (!encontrado && i<TAM_TBL) {
+		if (tabelaSimbolos[i] != NULL && tabelaSimbolos[i]->simbolo.lexema != NULL) {
+			if (strcmp(tabelaSimbolos[i]->simbolo.lexema, lexema) == 0) {
+				encontrado = 1;
+				retorno = tabelaSimbolos[i];
+			} else {
+				struct Celula *prox = tabelaSimbolos[i]->prox;
+				while (prox != NULL && !encontrado){
+					if (strcmp(prox->simbolo.lexema, lexema) == 0) {
+						encontrado = 1;
+						retorno = prox;
+					}
+					prox = prox->prox;
+				}
+			}
+		}
+		++i;
+	}
+	return retorno;
 }
 
 void adicionarReservados()
 {
-	adicionarRegistro("const");
-	adicionarRegistro("const");
-	adicionarRegistro("var");
-	adicionarRegistro("integer");
-	adicionarRegistro("char");
-	adicionarRegistro("for");
-	adicionarRegistro("if");
-	adicionarRegistro("else");
-	adicionarRegistro("and");
-	adicionarRegistro("or");
-	adicionarRegistro("not");
-	adicionarRegistro("=");
-	adicionarRegistro("to");
-	adicionarRegistro("(");
-	adicionarRegistro(")");
-	adicionarRegistro("<");
-	adicionarRegistro(">");
-	adicionarRegistro("<>");
-	adicionarRegistro(">=");
-	adicionarRegistro("<=");
-	adicionarRegistro(",");
-	adicionarRegistro("+");
-	adicionarRegistro("-");
-	adicionarRegistro("*");
-	adicionarRegistro("/");
-	adicionarRegistro(";");
-	adicionarRegistro("{");
-	adicionarRegistro("}");
-	adicionarRegistro("then");
-	adicionarRegistro("readln");
-	adicionarRegistro("step");
-	adicionarRegistro("write");
-	adicionarRegistro("writeln");
-	adicionarRegistro("%");
-	adicionarRegistro("[");
-	adicionarRegistro("]");
-	adicionarRegistro("do");
+	proc = adicionarRegistro("const",Identificador);
+	adicionarRegistro("var",Identificador);
+	adicionarRegistro("integer",Identificador);
+	adicionarRegistro("char",Identificador);
+	adicionarRegistro("for",Identificador);
+	adicionarRegistro("if",Identificador);
+	adicionarRegistro("else",Identificador);
+	adicionarRegistro("and",Identificador);
+	adicionarRegistro("or",Identificador);
+	adicionarRegistro("not",Identificador);
+	adicionarRegistro("=",Identificador);
+	adicionarRegistro("to",Identificador);
+	adicionarRegistro("(",Identificador);
+	adicionarRegistro(")",Identificador);
+	adicionarRegistro("<",Identificador);
+	adicionarRegistro(">",Identificador);
+	adicionarRegistro("<>",Identificador);
+	adicionarRegistro(">=",Identificador);
+	adicionarRegistro("<=",Identificador);
+	adicionarRegistro(",",Identificador);
+	adicionarRegistro("+",Identificador);
+	adicionarRegistro("-",Identificador);
+	adicionarRegistro("*",Identificador);
+	adicionarRegistro("/",Identificador);
+	adicionarRegistro(";",Identificador);
+	adicionarRegistro("{",Identificador);
+	adicionarRegistro("}",Identificador);
+	adicionarRegistro("then",Identificador);
+	adicionarRegistro("readln",Identificador);
+	adicionarRegistro("step",Identificador);
+	adicionarRegistro("write",Identificador);
+	adicionarRegistro("writeln",Identificador);
+	adicionarRegistro("%",Identificador);
+	adicionarRegistro("[",Identificador);
+	adicionarRegistro("]",Identificador);
+	adicionarRegistro("do",Identificador);
 }
 
 void inicializarTabela()
 {
+	/*
 	for(int i=0; i<TAM_TBL; ++i) {
 		struct Celula *cel = (struct Celula *) malloc(sizeof(struct Celula));
 		struct Simbolo *simb = (struct Simbolo *) malloc(sizeof(struct Simbolo));
 		cel->simbolo = *simb;
 		tabelaSimbolos[i] = cel;
 	}
+	*/
     adicionarReservados();
 }
 
@@ -155,6 +174,7 @@ int main(int argc, char *argv[])
 	}
 
 	inicializarTabela();
+	/*
 	for (int i=0; i<TAM_TBL; ++i) {
 		printf("%d - %s", i, tabelaSimbolos[i]->simbolo.lexema);
 		struct Celula *prox = tabelaSimbolos[i]->prox;
@@ -164,6 +184,10 @@ int main(int argc, char *argv[])
 		}
 		printf("\n");
 	}
+	*/
+	printf("pesquisando por const\n");
+	printf("%p, %p\n",pesquisarRegistro("const"), proc);
+
 
 	return 0;
 }
