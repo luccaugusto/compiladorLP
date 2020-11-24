d8c550d733788faf8c9490aeddbb4b76181994f9
lrr68 <luccaaugusto@protonmail.ch>
2020-11-24 09:42:49 -0300

    declaracao de funcoes que foi removida no ultimo commit

--- src/ts.c
+++ src/ts.c
@@ -7,6 +7,15 @@
 	#define TAM_TBL 254
 	//define DEBUG_TS 0
 
+	/* Tabela de símbolos */
+	Tipo buscaTipo(char *);
+	void limparTabela(void);
+	void inicializarTabela(void);
+	void adicionarReservados(void);
+	void mostrarTabelaSimbolos(void);
+	void limparLista(struct Celula *);
+	struct Celula *pesquisarRegistro(char *);
+	struct Celula *adicionarRegistro(char *, Tokens);
 
 	extern struct Celula *tabelaSimbolos[TAM_TBL];
 	
